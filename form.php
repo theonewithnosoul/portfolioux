07dbbeec7a3ab1113249d2e5fd00881cf32f2c09
Lucía León <leon.mmlucia@gmail.com>
2022-12-29 00:02:05 -0300

    php form

--- form.php
+++ form.php
@@ -1,25 +1,28 @@
 <?php
 
-$nombre = $_POST ['name'];
-$email = $_POST ['email'];
+$name = $_POST ['name'];
+$mail = $_POST ['email'];
 $subject = $_POST ['subject'];
 $message = $_POST ['message'];
 
 //como me va a llegar el cuerpo del mensaje
 
-$message = "Este mensaje fue enviado por" .$nombre. ",\r\n";
-$message = "Su e-mail es: " .$email. ",\r\n";
-$message = "El asunto es: " .$subject. ",\r\n";
-$message = "Mensaje: " . $_POST ['mensaje'] . ",\r\n";
-$message = "Enviado el " . date('d/m/y', time());
+$header = 'from' . $mail . ",\r\n";
+$header = "X-Mailer: PHP/" . phpversion() . ",\r\n";
+$header = "Content-Type: text-plain";
 
-$header = "Reply-to: leon.mmlucia@gmail.com"
-$header = "X-Mailer: PHP/" . phpversion();
 
-$destinatario = 'leon.mmlucia@gmail.com';
+$message = "Este mensaje fue enviado por" . $name . ",\r\n";
+$message = "Su e-mail es: " . $mail . ",\r\n";
+$message = "El asunto es: " . $subject . ",\r\n";
+$message = "Mensaje: " . $_POST['message'] . ",\r\n";
+$message = "Enviado el " . date('d/m/Y', time());
+
+
+$para = 'leon.mmlucia@gmail.com';
 $asunto = 'Este email fue enviado desde tu portfolio';
 
-$mail = @mail($destinatario,$asunto, utf8_decode($message),$header);
+$mail = mail($para, $asunto, utf8_decode($message), $header);
 
 if($mail) {
     echo "<h4> mail enviado </4>";
